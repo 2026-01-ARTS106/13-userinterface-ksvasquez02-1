7801d58c8a77c10e96e0f684c7070a9ab7a9d416
ShovelCrow <ksvasquez02@gmail.com>
2026-04-26 06:16:55 -0400

    Wenderlich Part 3

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -6,6 +6,8 @@ public class UIManager : MonoBehaviour
     public Animator startButton;
     public Animator settingsButton;
     public Animator dialog;
+    public Animator contentPanel;
+    public Animator gearImage;
 
     public void StartGame()
     {
@@ -24,5 +26,12 @@ public class UIManager : MonoBehaviour
         startButton.SetBool("isHidden", false);
         settingsButton.SetBool("isHidden", false);
         dialog.SetBool("isHidden", true);
+    }
+
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+        gearImage.SetBool("isHidden", !isHidden);
     }
 }
